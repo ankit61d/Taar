4804944137930afdd1a226b48daab82319419724
Ankit <ankit61d@gmail.com>
2022-05-24 12:23:22 +0530

    added 2 api_url_prefix routes - friends and register

--- app.py
+++ app.py
@@ -20,6 +20,28 @@ def api_login():
         mimetype='application/json')
     return response
 
+@app.route(f"/{api_url_prefix}/register", methods=['POST'])
+def api_register():
+    print(request.data)
+    data = {"message": "User registered"}
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json')
+    return response
+
+@app.route(f"/{api_url_prefix}/friends", methods=['GET'])
+def api_friends():
+    print(request.data)
+    data = {
+        "friends":[ {"name": "<name>", "userId": "userId1"},
+                    {"name": "<name>", "userId": "userId2"},]
+        }
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json')
+    return response
 
 # sample routes 
 @app.route(f"/")
